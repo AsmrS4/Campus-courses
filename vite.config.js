a968a51ed7d8a4a2a743e77ce93bb69fe5fa7efc
Aymir Sunchugashev <as4n.tsu@gmail.com>
2024-11-15 01:16:47 +0700

    added vite.config file

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import { ViteAliases } from "vite-aliases";
+import legacy from "@vitejs/plugin-legacy";
+
+
+export default defineConfig({
+    root: resolve(__dirname, 'src'),
+    server: {
+        port: 8080,
+        hmr: true,
+        proxy: {
+            '/login': {
+                target: 'http://localhost:8080/pages/account',
+                rewrite: (path) => path.replace(/^\/pages\/account\/login.html/, '')
+            },
+            '/register': {
+                target: 'http://localhost:8080/pages/account',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/pages\/account\/register.html/, '')
+            },
+            '/*': {
+                target: 'http://localhost:8080',
+                rewrite: (path) => path.replace(/^\/index.html/, '')
+            },
+            '/profile': {
+                target: 'http://localhost:8080/pages/account',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/pages\/account\/profile.html/, '')
+            }
+        }
+    },
+    
+})
