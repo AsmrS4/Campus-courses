0b150063707d690ff8cf3f6a07f15c09ed007643
Aymir Sunchugashev <as4n.tsu@gmail.com>
2024-11-18 23:31:52 +0700

    finished profile page

--- src/api/account/index.js
+++ src/api/account/index.js
@@ -13,18 +13,18 @@ export const registerNewUser = async (data) => {
             )
         })
 
-        if(response.ok) {
+        if (response.ok) {
             return response.json();
         } else {
-            if(response.status === 400) throw new Error('Bad request');
-            if(response.status === 500) throw new Error('Server Error');
+            if (response.status === 400) throw new Error('Bad request');
+            if (response.status === 500) throw new Error('Server Error');
         }
     } catch (error) {
         console.error('Fetch failed with', error)
     }
 }
 
-export const loginUser = async(data) => {
+export const loginUser = async (data) => {
     try {
         const response = await fetch('https://camp-courses.api.kreosoft.space/login', {
             method: "POST",
@@ -39,13 +39,69 @@ export const loginUser = async(data) => {
             )
         })
 
-        if(response.ok) {
+        if (response.ok) {
             return response.json();
         } else {
-            if(response.status === 400) throw new Error('Bad request');
-            if(response.status === 500) throw new Error('Server Error');
+            if (response.status === 400) throw new Error('Bad request');
+            if (response.status === 500) throw new Error('Server Error');
         }
 
+    } catch (error) {
+        console.log('Fetch failed with', error)
+    }
+}
+
+export const getUserProfile = async () => {
+    let token = localStorage.getItem('token');
+    try {
+        const response = await fetch('https://camp-courses.api.kreosoft.space/profile', {
+            method: "GET",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': "Bearer " + token
+            }
+        })
+        if (response.ok) {
+            return response.json();
+        } else {
+            if (response.status === 401) {
+                localStorage.clear();
+                window.location.href = '/'
+                throw new Error('Unathorized')
+            }
+            if (response.status === 500) throw new Error('Server Error');
+        }
+
+    } catch (error) {
+        console.log('Fetch failed with', error)
+    }
+}
+
+export const editUserProfile = async (data) => {
+    let token = localStorage.getItem('token');
+    try {
+        const response = await fetch('https://camp-courses.api.kreosoft.space/profile', {
+            method: 'PUT',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': "Bearer " + token
+            },
+            body: JSON.stringify({
+                ...data
+            })
+        })
+        if (response.ok) {
+            return response.json();
+        } else {
+            if (response.status === 401) {
+                localStorage.clear();
+                window.location.href = '/'
+                throw new Error('Unathorized')
+            }
+            if (response.status === 500) throw new Error('Server Error');
+        }
     } catch (error) {
         console.log('Fetch failed with', error)
     }
--- src/js/account/profile.js
+++ src/js/account/profile.js
@@ -1,2 +1,50 @@
 import '../../scss/styles.scss'
 import * as bootstrap from 'bootstrap';
+import $ from 'jquery';
+import { editUserProfile, getUserProfile } from '../../api/account';
+import { birthDateIsValid } from '../utils/validation';
+
+
+const getProfile = async () => {
+    const result = await getUserProfile();
+    if (result) {
+        console.log()
+        $('#login-input').val(result.email);
+        $('#username-input').val(result.fullName);
+        $('#birthday-input').val(result.birthDate.slice(0, 10));
+    }
+}
+
+const editProfile = async () => {
+    $('#birthday-input').removeClass('is-invalid');
+    $('#birthday-error-label').addClass('d-none');
+    
+    let birthDate = $('#birthday-input').val();
+    let userName = $('#username-input').val();
+
+    if (birthDateIsValid(birthDate)) {
+        const result = await editUserProfile({
+            fullName: userName,
+            birthDate: birthDate
+        })
+        if (result) {
+            getProfile();
+        }
+    } else {
+        if (!birthDateIsValid(birthDate)) {
+            $('#birthday-input').addClass('is-invalid');
+            $('#birthday-error-label').removeClass('d-none');
+        }
+    }
+}
+
+$(window).ready(() => {
+    getProfile();
+})
+
+$('#profile-form').submit(e => {
+    e.preventDefault();
+    if ($('#profile-form')[0].checkValidity()) {
+        editProfile()
+    }
+})
